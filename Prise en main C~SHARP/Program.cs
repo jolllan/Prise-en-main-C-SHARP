788b60b13a7b276672bd73692af1c64b8420562d
jolllan <72869160+jolllan@users.noreply.github.com>
2023-02-08 15:01:21 +0100

    Ajout de deux hello world et classe

--- Prise en main C~SHARP/Program.cs
+++ Prise en main C~SHARP/Program.cs
@@ -2,4 +2,20 @@
 
 
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+
+
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+     Console.WriteLine("Hello World!");
+     int coucou = 15;
+     Console.WriteLine($"Heybey : {coucou}");
+    }
+  }
+}
